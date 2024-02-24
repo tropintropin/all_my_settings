a4ee5dd751f5e7a63c3d96c4d9a2c9df9f27b70c
Valery Tropin <tropin.tropin@gmail.com>
2024-02-24 15:06:31 +0300

    Add color prompt to script
    Fix:
        create_config_symlinks.sh

--- create_config_symlinks.sh
+++ create_config_symlinks.sh
@@ -8,6 +8,23 @@
 # Затем запустите скрипт:
 # ./create_config_symlinks.sh
 
+# Функция для цветного вывода текста. 1 аргумент — цвет, 2 — текст
+    # BLACK       0
+	# RED         1
+	# GREEN       2
+	# YELLOW      3
+	# BLUE        4
+	# PURPLE      5
+	# CYAN        6
+	# WHITE       7
+colorecho() {
+    local color="$1"
+    local message="$2"
+    tput setaf "$color"
+    echo "$message"
+    tput sgr0
+}
+
 # Определение абсолютного пути до директории, в которой находится скрипт
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -19,13 +36,13 @@ function check_and_create_symlink() {
 
     if [ -f "$symlink_path" ]; then
         if [ -L "$symlink_path" ] && [ "$(readlink "$symlink_path")" = "$source_path" ]; then
-            echo "Симлинк $symlink_path уже указывает на файл $source_path"
+            colorecho 6 "Симлинк $symlink_path уже указывает на файл $source_path"
         else
-            echo "Симлинк $symlink_path уже существует и будет перезаписан"
+            colorecho 1 "Симлинк $symlink_path уже существует и будет перезаписан"
             ln -sf "$source_path" "$symlink_path"
         fi
     else
-        echo "Создаю симлинк $symlink_path"
+        colorecho 3 "Создаю симлинк $symlink_path"
         ln -s "$source_path" "$symlink_path"
     fi
 }
@@ -35,8 +52,8 @@ files=("bashrc" "gitconfig" "zshrc" "vimrc" "tmux.conf")
 
 # Цикл для проверки и создания симлинков
 for file in "${files[@]}"; do
-    echo "Проверка файла $file..."
+    colorecho 7 "Проверка файла $file..."
     check_and_create_symlink ".$file"
 done
 
-echo "Скрипт завершен."
+colorecho 2 "Скрипт завершен!"
